e0147d32eab3974733c8d652b473329c26e3997a
ZeR0 <99215787+ZeR0vi@users.noreply.github.com>
2023-01-17 11:13:56 -0300

    Add files via upload

---
+++ mobile/App.tsx
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+
+import { useFonts, 
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold } from '@expo-google-fonts/inter'
+import { Loading } from './src/components/loading';
+
+export default function App() {
+
+ const [fontsLoaed] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  })
+
+  if(!fontsLoaed) {
+   return (
+    <Loading/>
+   )
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.tex}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar barStyle={'light-content'} backgroundColor="transparent" translucent />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#09090A',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tex: {
+    color: 'white',
+    fontFamily: 'Inter_800ExtraBold'
+  },
+});
